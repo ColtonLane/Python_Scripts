67c1d1c8a480523d306e4f2a847e2c99028b2203
ColtonLane <coltonlane314@gmail.com>
2024-02-17 12:22:04 -0500

    Works, plus built-in list for "ALL" option
    Now adds files to folder based on extension type. Also checks through a typeList when the user inputs "ALL", adds all types on the list to their respective folders

--- fileOrganizer/fileOrg.py
+++ fileOrganizer/fileOrg.py
@@ -5,28 +5,41 @@ import shutil
 from subprocess import PIPE, run
 
 def findFilesOfType(type, source): 
-    file_paths = []
-    if os.path.isdir("{}\\{}".format(source, type)) == False: #checks if a folder of name "type" exists; 
-        os.makedirs("{}".format(type))
-        print(os.listdir(source))
-        pass
-
+    fileList = []
     for root, dirs, files in os.walk(source): 
-        for directory in dirs: 
-            if directory.endswith(".{}".format(type)): #checks if string ends in "." followed by specified path (avoids replacing none-type periods by mistake)
-                curPath = os.path.join(source, directory) #combines source directory and files ending in given type
-                file_paths.append(curPath) 
+        for file in files: 
+            if file.endswith(".{}".format(type)) or file.endswith(".{}".format(type.upper())) or file.endswith(".{}".format(type.lower())): #checks if string ends in "." followed by specified path (avoids replacing none-type periods by mistake)
+                curPath = os.path.join(source, file) #combines source directory and files ending in given type
+                fileList.append(curPath)
         break 
-    return file_paths
+    return fileList 
+
+def addToFolder(fileList, type, source): 
+    folderPath = os.path.join("{}\\{}".format(source, type))
+    if os.path.isdir(folderPath) == False: #checks if a folder of name "type" exists; 
+        os.makedirs(folderPath)
+    for i in fileList: 
+        shutil.move(i, folderPath)
+    if len(os.listdir(folderPath)) == 0: 
+        shutil.rmtree(folderPath)
+
+def findAllFileTypes(source): 
+    typeList = ["png", "jpg", "gif", "svg", "tiff", "tif", "docx", "doc", "html", "xlsx", "ppxt", "mp4", "avi", "MOV", "flv", "avchd", 
+                "pptx", "odp", "key", "m4a", "mp3", "wav", "exe", "bat", "rtf", "aseprite", "jpeg", "txt", "pdf", "py", "java", "css", 
+                "cs", "cpp", "iso", "aseprite-extension", "z64", "7z", "json", "zip", "sf2", "ipynb", "lss", "msi", "webp"]
+    for i in typeList: 
+        addToFolder(findFilesOfType(i, source), i, source)
 
 def main(source, fileType): 
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     fileType.replace(".", "")
-    file_paths = findFilesOfType(fileType, source_path)
 
-    if file_paths == None: 
-        print("No files of type '{}' found.".format(fileType))
+    if (fileType == "ALL"): 
+        findAllFileTypes(source)
+    else: 
+        addToFolder(findFilesOfType(fileType, source_path), fileType, source)
+
 
 if __name__ == "__main__": 
     args = sys.argv
